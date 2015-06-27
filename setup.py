01db2d7747fb315c43ccddadf1139e7698ab3299
Sheikh Araf <arafsheikh@rocketmail.com>
2015-06-28 02:21:30 +0530

    Added install_requires attribute & more

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
 from setuptools import setup
+import os
+
+#Function to read README
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='clipboard_memo',
     version='0.1',
-    description='Command-line clipboard manager',
+    description='A command-line clipboard manager',
+    long_description=read('README.md'),
     url='http://github.com/arafsheikh/clipboard-memo',
     author='Sheikh Araf',
     author_email='arafsheikh@rocketmail.com',
     license='MIT',
+    keywords='clipboard memo manager command-line CLI',
     include_package_data=True,
     entry_points='''
         [console_scripts]
-        cman=clipboard_memo:main
+        cmemo=clipboard_memo:main
+        cmemo_direct=clipboard_memo:direct_save
     ''',
+    py_modules=['clipboard_memo'],
+    install_requires=[
+        'pyperclip',
+    ],
 )
